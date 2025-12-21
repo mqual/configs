9b1f2b62933ba07e66564d0c3da67ecdfd0a6126
Vishal <95016059+haxfn@users.noreply.github.com>
2025-12-21 23:23:10 +0530

    Update config.fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,61 +1,66 @@
+setenv EDITOR $HOME/.local/bin/ed
+setenv BROWSER /usr/bin/microsoft-edge-beta
+
+
+
+
+
+# toolchains
+source "$HOME/.cargo/env.fish"
+
+if test -e /nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh
+    set -gx PATH /nix/var/nix/profiles/default/bin $PATH
+    set -gx NIX_PATH $HOME/.nix-defexpr/channels
+    set -gx NIX_PROFILES "/nix/var/nix/profiles/default $HOME/.nix-profile"
+    set -gx NIX_SSL_CERT_FILE /nix/var/nix/profiles/default/etc/ssl/certs/ca-bundle.crt
+    set -gx PATH $HOME/.nix-profile/bin $PATH
+end
+
+direnv hook fish | source
+
+
+
+#set -gx PATH "$HOME/.miniconda/bin" $PATH
+#set -gx PATH "$HOME/.juliaup/bin" $PATH
+#set -x LD_LIBRARY_PATH /usr/lib $LD_LIBRARY_PATH
+#source "/home/haxfn/Repos/emsdk/emsdk_env.fish"
+
+# note: this does eval (opam env) alongside some variable set up 
+# BEGIN opam configuration
+# This is useful if you're using opam as it adds:
+#   - the correct directories to the PATH
+#   - auto-completion for the opam binary
+# This section can be safely removed at any time if needed.
+# test -r '/home/haxfn/.opam/opam-init/init.fish' && source '/home/haxfn/.opam/opam-init/init.fish' > /dev/null 2> /dev/null; or true
+# END opam configuration
+
+#set -q GHCUP_INSTALL_BASE_PREFIX[1]; or set GHCUP_INSTALL_BASE_PREFIX $HOME ; set -gx PATH $HOME/.cabal/bin /Users/haxfn/.ghcup/bin $PATH # ghcup-env
+
+
+
+
+function fish_user_key_bindings
+    # fish_vi_key_bindings
+    bind \ck kill-line
+end
+
+
 if status --is-interactive
-    abbr -a g git
-    abbr -a ga 'git add -p'
-    abbr -a gc 'git checkout'
-    abbr -a gs 'git status'
-    abbr -a gdiff 'git diff'
+    alias wd 'cd (cat $HOME/.cache/workingdir)'
     abbr -a gd "cd (git rev-parse --show-toplevel)"
     abbr -a ed '$EDITOR'
-    abbr -a l eza
+
     abbr -a la 'eza -la --group-directories-last'
-    abbr -a ll 'eza -l'
-    abbr -a ls eza
-    abbr -a o xdg-open
-    abbr -a p 'sudo pacman'
-    abbr -a pi 'sudo pacman -S'
-    abbr -a pu 'sudo pacman -Syu'
-    abbr -a dl trash
+    abbr -a ls `eza -a1`
+
+    # note: need to move this to be in make files
     abbr -a gcc 'gcc -Wall -Wextra -Wshadow -std=c23 -O2 -o main'
     abbr -a g++ 'g++ -Wall -Wextra -Wshadow -std=c++23 -O2 -o main'
-    # abbr -a y yazi
-    function y
-        set tmp (mktemp -t "yazi-cwd.XXXXXX")
-        yazi $argv --cwd-file="$tmp"
-        if read -z cwd <"$tmp"; and [ -n "$cwd" ]; and [ "$cwd" != "$PWD" ]
-            builtin cd -- "$cwd"
-        end
-        rm -f -- "$tmp"
-    end
-    
-    alias wd 'cd (cat $HOME/.cache/workingdir)'
-    alias clear 'tput cup $LINES; commandline ""'
-    function fish_user_key_bindings
-        # fish_vi_key_bindings
-        # bind \cl 'clear; commandline -f repaint'
-        bind \ck kill-line
-        bind \ey y
-        bind \el 'clear; eza; commandline -f repaint;'
-    end
 
     # set -Ux CARAPACE_BRIDGES 'zsh,fish,bash,inshellisense' # optional
     # carapace _carapace | source
 
-    # lang-toolchains
-    #set -gx PATH "$HOME/.miniconda/bin" $PATH
-    source "$HOME/.cargo/env.fish"
-    #opam env | source
-    #set -gx PATH "$HOME/.juliaup/bin" $PATH
-    set -gx PATH "$HOME/.local/bin" $PATH
-    #set -x LD_LIBRARY_PATH /usr/lib $LD_LIBRARY_PATH
 
-    
-    # source "/home/haxfn/Repos/emsdk/emsdk_env.fish"
-
-    setenv EDITOR $HOME/.local/bin/ed
-    setenv DOTFILES $HOME/Documents/configs
-    setenv BROWSER /usr/bin/microsoft-edge-stable
-    
-    starship init fish | source
 
     # Fish git prompt
     set __fish_git_prompt_showuntrackedfiles yes
@@ -65,16 +70,50 @@ if status --is-interactive
     set -g fish_prompt_pwd_dir_length 1
     set __fish_git_prompt_shorten_branch_len 15
 
+    #starship init fish | source
+
     # theming
-    sh ~/.config/fish/themes/base16-github-light.sh
+    #sh ~/.config/fish/themes/base16-apathy.sh
 end
 
-# BEGIN opam configuration
-# This is useful if you're using opam as it adds:
-#   - the correct directories to the PATH
-#   - auto-completion for the opam binary
-# This section can be safely removed at any time if needed.
-test -r '/home/haxfn/.opam/opam-init/init.fish' && source '/home/haxfn/.opam/opam-init/init.fish' > /dev/null 2> /dev/null; or true
-# END opam configuration
 
-# note to self: this does eval (opam env) alongside some variable set up 
+
+function fish_prompt
+    # Left: [ haxfn ] in bold, white brackets
+    set_color -d white
+    echo -n "[ "
+    set_color normal
+    set_color -o green
+    echo -n (whoami)
+    set_color normal
+    set_color -d white
+    echo -n " ] ₹ "
+    set_color normal
+end
+
+function fish_right_prompt
+    set_color blue
+    #echo -n (date +'%H:%M:%S')
+    echo (math "floor($CMD_DURATION / 1000 / 60)")"m"
+    set_color white
+    echo -n " : "
+    set_color red
+    echo -n (fish_git_prompt)
+    echo -n " : "
+    set_color white
+    echo -n (prompt_pwd)
+    set_color white
+    echo -n " : "
+    set_color blue
+    echo -n "( "
+    set_color -o green
+    echo -n (hostname -s)
+    set_color normal
+    set_color blue
+    echo -n " )"
+    set_color normal
+end
+
+
+
+# Vishal (@haxfn)
